f673ce3730039b9e9f3bb8f73776bdbd80beea99
Irwant Siregar <irwantosiregar@synapsis.id>
2024-01-19 15:10:36 +0700

    crud feature todo app __

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-floating-promises */
 import React, { useEffect, useState, type ChangeEvent } from 'react';
-import { MdOutlineCheck, MdDeleteOutline, MdCreate } from 'react-icons/md';
+import { MdOutlineCheck, MdDeleteOutline, MdCreate, MdClose } from 'react-icons/md';
 import { receiveAllTodo, deleteTodoById, addOneTodo, updateOneTodo } from './features/todos';
 
 interface Todos {
@@ -20,6 +20,7 @@ type Action =
 
 function App (): React.ReactElement {
   const [todos, setTodos] = useState<Todos[] | []>([]);
+  const [tabsTodo, setTabsTodo] = useState<Todos[] | []>([]);
   const [newTodo, setNewTodo] = useState<string>('');
   const [selectedTodo, setSelectedTodo] = useState<number | string>('');
   const [action, setAction] = useState<Action>({ type: 'add' });
@@ -28,6 +29,7 @@ function App (): React.ReactElement {
     (async function () {
       const data = await receiveAllTodo();
       setTodos(data.todos);
+      setTabsTodo(data.todos);
     })();
   }, []);
 
@@ -75,11 +77,11 @@ function App (): React.ReactElement {
 
   const handleUpdateTabs = (tab: Action): void => {
     if (tab.type === 'completed') {
-      setTodos(todos.filter((item) => item.completed));
+      setTodos(tabsTodo.filter((item) => item.completed));
     } else if (tab.type === 'active') {
-      setTodos(todos.filter((item) => !item.completed));
+      setTodos(tabsTodo.filter((item) => !item.completed));
     } else {
-      setTodos(todos);
+      setTodos(tabsTodo);
     }
   };
 
@@ -92,9 +94,9 @@ function App (): React.ReactElement {
           <div>
             <h1 className="text-white text-5xl font-semibold tracking-[0.2em]">TODO</h1>
           </div>
-          <div className="flex justify-between p-6 bg-slate-200 rounded-md">
+          <div className="flex justify-between rounded-md">
             <div className="w-3/4 pr-2">
-              <input value={newTodo} onChange={handleChangeInputTodo} type="text" className="w-full bg-white text-xl text-dark mt-1 px-4 py-3 shadow-lg rounded-lg focus:outline-none focus:ring-primary focus:ring-1 focus:border-primary"/>
+              <input value={newTodo} onChange={handleChangeInputTodo} type="text" placeholder='Input your todo' className="w-full bg-white text-xl text-dark mt-1 px-4 py-3 shadow-lg rounded-lg focus:outline-none focus:ring-primary focus:ring-2 focus:border-primary"/>
             </div>
               <button onClick={() => { handleSave(); }} className="text-base w-1/4 md:w-64 font-semibold text-white bg-sky-500 py-3 px-8 rounded-full shadow-lg hover:opacity-80 hover:shadow-lg transition duration-500">{action.type === 'update' ? 'Update' : 'Create'}</button>
           </div>
@@ -141,9 +143,19 @@ function App (): React.ReactElement {
                   <div className="flex justify-between px-2 py-3 border-t border-slate-300 text-xl hover:bg-slate-100" key={index + 1}>
                     <p>{item.todo}</p>
                     <div className="flex gap-4">
-                      <div className="flex items-center p-2 border border-primary rounded hover:cursor-pointer">
-                        <MdOutlineCheck className="text-primary" />
-                      </div>
+                      {
+                        !item.completed
+                          ? (
+                          <div className="flex items-center p-2 border border-primary rounded hover:cursor-pointer">
+                            <MdOutlineCheck className="text-primary" />
+                          </div>
+                            )
+                          : (
+                          <div className="flex items-center p-2 border border-red-500 rounded hover:cursor-pointer">
+                            <MdClose className="text-red-500" />
+                          </div>
+                            )
+                    }
                       <div onClick={() => { handleSelected(item); } } className="flex items-center p-2 border border-primary rounded hover:cursor-pointer">
                         <MdCreate className="text-primary" />
                       </div>
